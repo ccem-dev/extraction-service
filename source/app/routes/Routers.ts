a57c258eb17b653ee656c8e4d2f87d0fbce96ccb
FlaviaAP <flavia.avila@ufrgs.br>
2021-01-05 18:38:46 -0300

    ES-4 add PipelineRouter em Routers

--- source/app/routes/Routers.ts
+++ source/app/routes/Routers.ts
@@ -1,7 +1,9 @@
-import { Router } from 'express'
+import { Router } from 'express';
 import ExtractionRouter from "../routes/ExtractionRouter";
+import PipelineRouter from "../routes/PipelineRouter";
 
-const routes = Router()
-routes.use("/extractions", ExtractionRouter)
+const routes = Router();
+routes.use("/extractions", ExtractionRouter);
+routes.use("/pipeline", PipelineRouter);
 
-export default routes
+export default routes;
