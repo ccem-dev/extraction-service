d028a8856ff973185557be68d070dac6af14ad1f
FlaviaAP <flavia.avila@ufrgs.br>
2021-01-07 00:11:36 -0300

    ES-4 método performAsCsv em performAsCsv

--- source/app/controllers/PipelineController.ts
+++ source/app/controllers/PipelineController.ts
@@ -10,6 +10,15 @@ class PipelineController {
         res.status(err.code).send(err.body)
       });
   }
+
+  performAsCsv(req: Request, res: Response): void {
+    PipelineService.performAsCsv(req.params.pipelineName)
+      .then(result => res.status(result.code).send(result.body))
+      .catch(err => {
+        console.error('erro:', err);
+        res.status(err.code).send(err.body)
+      });
+  }
 }
 
 export default new PipelineController();
