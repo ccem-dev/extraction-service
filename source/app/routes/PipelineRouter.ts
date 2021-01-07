b60afed7484d52ba3a3c4e922e7799b98fab0789
FlaviaAP <flavia.avila@ufrgs.br>
2021-01-07 00:12:04 -0300

    ES-4 add rota "/csv/:pipelineName" em PipelineRouter

--- source/app/routes/PipelineRouter.ts
+++ source/app/routes/PipelineRouter.ts
@@ -3,5 +3,6 @@ import PipelineController from "../../app/controllers/PipelineController";
 
 const  PipelineRouter = Router();
 PipelineRouter.get("/json/:pipelineName", PipelineController.performAsJson);
+PipelineRouter.get("/csv/:pipelineName", PipelineController.performAsCsv);
 
 export default PipelineRouter;
