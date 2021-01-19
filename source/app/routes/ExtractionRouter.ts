582f6dabad47a2873dcf5791d62fc3046cff1de1
adonisgarcia <adonis.garcia.adg@gmail.com>
2021-01-19 17:16:20 -0300

    ES-3 rename de url da requisição

--- source/app/routes/ExtractionRouter.ts
+++ source/app/routes/ExtractionRouter.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import ExtractionController from "../../app/controllers/ExtractionController";
 const  ExtractionRouter = Router()
-ExtractionRouter.put("/activity/create", ExtractionController.create)
-ExtractionRouter.delete("/activity/remove/:surveyId/:activityId", ExtractionController.remove)
+ExtractionRouter.put("/activity", ExtractionController.create)
+ExtractionRouter.delete("/activity/:surveyId/:activityId", ExtractionController.remove)
 
 export default ExtractionRouter
