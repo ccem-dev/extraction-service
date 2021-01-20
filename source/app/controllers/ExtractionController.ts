74d9ceede1353d7f6286f0aa6a10e0bbeec2e5ec
adonisgarcia <adonis.garcia.adg@gmail.com>
2021-01-20 10:48:10 -0300

    ES-3 teste de unidade controller

--- source/app/controllers/ExtractionController.ts
+++ source/app/controllers/ExtractionController.ts
@@ -2,14 +2,14 @@ import ActivityExtractionService from "../services/ActivityExtractionService";
 import { Request, Response } from "express";
 
 class ExtrationController {
-  create(req: Request, res: Response): void {
-    ActivityExtractionService.create(req.body)
+  async create(req: Request, res: Response): Promise<void> {
+    await ActivityExtractionService.create(req.body)
       .then(result => res.status(result.code).send(result.body))
       .catch(err => res.status(err.code).send(err.body));
   }
 
-  remove(req: Request, res: Response): void {
-    ActivityExtractionService.remove(req.params.surveyId, req.params.activityId)
+  async remove(req: Request, res: Response): Promise<void> {
+    await ActivityExtractionService.remove(req.params.surveyId, req.params.activityId)
       .then(result => res.status(result.code).send(result.body))
       .catch(err => res.status(err.code).send(err.body));
   }
