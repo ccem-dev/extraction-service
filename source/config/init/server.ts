40dc8c3ed4832ff2d725824ecc303c6f260a0dbe
FlaviaAP <flavia.avila@ufrgs.br>
2021-01-25 10:56:27 -0300

    ES-4 server sem conexão com mongo

--- source/config/init/server.ts
+++ source/config/init/server.ts
@@ -1,45 +1,27 @@
 import App from "./app";
-import mongoose from "mongoose"
+import { Application } from "express"
 const listEndpoints = require('express-list-endpoints');
 
 class Server {
 
   constructor() {
-    this.connect()
-    this.listen()
-  }
-
-  connect() {
-    const options = {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      connectTimeoutMS: 1000,
-      auth: {
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASS
-      },
-    };
-
-    const url = `mongodb://${process.env.DATABASE_HOSTNAME}:${process.env.DATABASE_PORT}/${process.env.DATABASE}?authSource=${process.env.DATABASE}`;
-
-    console.log("connecting to: " + url);
-
-    mongoose.connect(url, options)
-      .then(response => console.info("mongodb connected"))
-      .catch(error => console.info(error))
+    this.listen();
   }
 
   listen() {
     App.listen(process.env.API_PORT, () => {
     });
-    console.info(process.env.API_PORT)
+    console.info(process.env.API_PORT);
     this.endpointsList();
   }
 
   endpointsList() {
     let endpoints = listEndpoints(App);
-    console.table(endpoints)
+    console.table(endpoints);
+  }
+
+  public getApp(): Application {
+    return App;
   }
 }
 export default new Server();
