78133c4a8542b284f6c3b908c89ff346876d09db
FlaviaAP <flavia.avila@ufrgs.br>
2021-01-25 18:01:25 -0300

    ES-4 excluídos Extraction scripts não usados
    para nao conflitar com os de ES-3

--- source/app/routes/ExtractionRouter.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import ExtractionController from "../../app/controllers/ExtractionController";
-const  ExtractionRouter = Router()
-ExtractionRouter.post("/create/:activityId", ExtractionController.create)
-ExtractionRouter.delete("/remove/:activityId", ExtractionController.remove)
-
-export default ExtractionRouter
--- source/app/routes/Routers.ts
+++ source/app/routes/Routers.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
-import ExtractionRouter from "../routes/ExtractionRouter";
 import PipelineRouter from "../routes/PipelineRouter";
 import RscriptRouter from "./RscriptRouter";
 
 const routes = Router();
-routes.use("/extractions", ExtractionRouter);
 routes.use("/pipeline", PipelineRouter);
 routes.use("/rscript", RscriptRouter);
 
