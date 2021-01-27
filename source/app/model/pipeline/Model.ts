4c790515ed94cc5e795e8e36b9d4920225627ab7
FlaviaAP <flavia.avila@ufrgs.br>
2021-01-26 23:29:48 -0300

    ES-4 remoção dos arquivos Model e Schema para pipeline e rscript
    não mais necessários pois não serviço não  se conecta mais com Mongo

--- source/app/model/pipeline/Model.ts
+++
@@ -1,6 +0,0 @@
-import Pipeline from "./Schema";
-import { getModelForClass } from '@typegoose/typegoose';
-
-const PipelineModel = getModelForClass(Pipeline);
-
-export default PipelineModel;
--- source/app/model/rscript/Model.ts
+++
@@ -1,6 +0,0 @@
-import Rscript from "./Schema";
-import {getModelForClass} from "@typegoose/typegoose";
-
-const RscriptModel = getModelForClass(Rscript);
-
-export default RscriptModel;
