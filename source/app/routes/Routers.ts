f766ea5be5682a1009f5bd4e2aac2d49581b6a21
FlaviaAP <flavia.avila@ufrgs.br>
2021-01-27 00:02:29 -0300

    ES-4 correção do merge em Routers

--- source/app/routes/Routers.ts
+++ source/app/routes/Routers.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import ExtractionRouter from "../routes/ExtractionRouter";
 import PipelineRouter from "../routes/PipelineRouter";
 import RscriptRouter from "./RscriptRouter";
 
 const routes = Router();
+routes.use("/extractions", ExtractionRouter);
 routes.use("/pipeline", PipelineRouter);
 routes.use("/rscript", RscriptRouter);
 
