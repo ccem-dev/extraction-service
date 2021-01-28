de57bf8ce77a17e61c5b7d18ac38d4451f30f80b
FlaviaAP <flavia.avila@ufrgs.br>
2021-01-28 14:17:59 -0300

    ES-4 métodos para buscar ids de survey(s)

--- source/app/services/SurveyService.ts
+++ source/app/services/SurveyService.ts
@@ -3,6 +3,7 @@ import ElasticsearchService from "./ElasticsearchService";
 import RscriptService from "./RscriptService";
 import CsvService from "../utils/CsvService";
 import ActivityExtractionService from "./ActivityExtractionService";
+import SurveyActivityIdsFactory from "../models/activity/SurveyActivityIdsFactory";
 
 const json2csv = require('json-2-csv').json2csvAsync;
 const axios = require('axios').default;
@@ -40,6 +41,46 @@ class SurveyService {
       let response = await json2csv(await findSurveyExtractions(surveyId),
         {delimiter: { field: CsvService.getDelimiter() }});
       return new SuccessResponse(await CsvService.createCsvFromString(response));
+    }
+    catch (e) {
+      return new NotFoundResponse(e);
+    }
+  }
+
+  async getSurveyActivitiesIds(surveyId: string) : Promise<IResponse>{
+    try {
+      let activityIds = (await findSurveyExtractions(surveyId))
+        .map((doc: { activityId: string }) => doc.activityId);
+      return new SuccessResponse(activityIds);
+    }
+    catch (e) {
+      return new NotFoundResponse(e);
+    }
+  }
+
+  async getAllActivitiesIds(): Promise<IResponse> {
+    try {
+      const SIZE = 10000;
+      const SCROLL_TIME = "1m";
+      const indexName = ActivityExtractionService.getIndexName('*');
+
+      let surveyIdDict : any = {};
+      let body = await firstSearch(indexName, SIZE, SCROLL_TIME);
+
+      while(body.hits && (body.hits.hits.length)) {
+        body.hits.hits.forEach((hit: {_index:string, _source: { activityId: string }}) => {
+          let surveyId = ActivityExtractionService.extractSurveyIdFromIndexName(hit._index);
+          if(!surveyIdDict[surveyId]){
+            surveyIdDict[surveyId] = [];
+          }
+          surveyIdDict[surveyId].push(hit._source.activityId);
+        });
+
+        body = await searchMore(body._scroll_id, SCROLL_TIME);
+      }
+
+      return new SuccessResponse(
+        Object.keys(surveyIdDict).map(surveyId => new SurveyActivityIdsFactory(surveyId, surveyIdDict[surveyId])));
     }
     catch (e) {
       return new NotFoundResponse(e);
