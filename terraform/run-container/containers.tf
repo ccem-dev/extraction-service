d3fb4666981de647c43cbd446cd9bfae62f3dd44
leogut <leogut@gmail.com>
2021-02-01 19:35:25 -0300

    ES-4 Terraform: default vars related to plumber.

--- terraform/run-container/containers.tf
+++ terraform/run-container/containers.tf
@@ -1,12 +1,18 @@
-variable "extraction-service-port"{ default = 51020 }
-variable "extraction-service-name"{ default = "extraction-service:latest" }
-variable "extraction-service-network"{ default = "otus-api-network" }
-variable "extraction-service-memory"{ default = "1024" }
+variable "extraction-service-memory"{ default = "4096" }
 variable "api-port"{ default = "8080" }
 variable "elasticsearch-hostname"{ default = "elasticsearch" }
 variable "elasticsearch-port"{ default = "9200" }
 variable "elasticsearch-protocol"{ default = "http" }
 variable "elasticsearch-initialize"{ default = "false" }
+variable "plumber-protocol"{ default = "http" }
+variable "plumber-hostname"{ default = "r-service" }
+variable "plumber-port"{ default = "8000" }
+variable "plumber-runner"{ default = "runner" }
+variable "extraction-service-port"{ default = 51020 }
+variable "extraction-service-name"{ default = "extraction-service:latest" }
+variable "extraction-service-network"{ default = "otus-api-network" }
+variable "extraction-max-content"{ default = "200000000" }
+variable "extraction-max-body"{ default = "200000000" }
 resource "docker_image" "extraction-service" { name = var.extraction-service-name }
 
 resource "null_resource" "network-creation" {
@@ -38,6 +44,12 @@ resource "docker_container" "extraction-service" {
     "ELASTICSEARCH_PORT=${var.elasticsearch-port}",
     "ELASTICSEARCH_PROTOCOL=${var.elasticsearch-protocol}",
     "ELASTICSEARCH_INITIALIZE=${var.elasticsearch-initialize}",
+    "PLUMBER_PROTOCOL=${var.plumber-protocol}",
+    "PLUMBER_HOSTNAME=${var.plumber-hostname}",
+    "PLUMBER_PORT=${var.plumber-port}",
+    "PLUMBER_RUNNER=${var.plumber-runner}",
+    "MAX_CONTENT_LENGTH=${var.extraction-max-content}",
+    "MAX_BODY_LENGTH=${var.extraction-max-body}"
   ]
   ports {
     internal = 8080
